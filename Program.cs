4e5c3e3191d9f9ee3f8a6b03d6ebf21fac9bfd1b
Maria <bachuri@ukr.net>
2024-10-12 03:42:58 +0300

    Регистрация в DI контейнере Регистрация в DI контейнере необходима, чтобы классы, такие как HomeController, могли получать реализацию интерфейса IRepository через конструктор, и таким образом система могла инжектировать зависимости автоматически. Использование Scoped времени жизни гарантирует, что для каждого HTTP-запроса создаётся новый экземпляр Repository

--- Program.cs
+++ Program.cs
@@ -1,27 +1,47 @@
+using GuestBookGit.Interfaces;
+
+using GuestBookRepos.Repositories;
+using GuestBookRepos.Service;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GuestBookGit.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSession();
+builder.Services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<PasswordHasherScript>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseSession();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "admin",
+    pattern: "{controller=Admin}/{action=RehashPasswords}");
 
 app.Run();
